f783c118553f1c92552e644d05ed79350e991808
Manya <manya@example.com>
2025-06-11 18:03:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function contentShow() {
+    const content = document.getElementById('addcontent');
+    const button = document.getElementById('contentshow');
+
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        button.textContent = 'Show Less';
+
+    } else {
+        content.style.display = 'none';
+        button.textContent = 'Show More'; 
+    }
+
+}
+
+function regards() {
+    alert('Thank you for your message!');
+}
