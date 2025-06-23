3a6684e2cc62bd9d045ada6b97dbef94cc6b46a6
Manya <manya@example.com>
2025-06-23 11:27:37 +0530

    Packages

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-function contentShow() {
-    const content = document.getElementById('addcontent');
-    const button = document.getElementById('contentshow');
 
-    if (content.style.display === 'none') {
-        content.style.display = 'block';
-        button.textContent = 'Show Less';
-
-    } else {
-        content.style.display = 'none';
-        button.textContent = 'Show More'; 
+    function showAlert() {
+        alert('The package has been chosen.');
     }
 
-}
 
-function regards() {
-    alert('Thank you for your message!');
-}
